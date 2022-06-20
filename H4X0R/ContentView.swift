a0f53eb8df7dc64b2e6aaccc294a7aedd0173aa5
tomoaki-narita <output.org@gmail.com>
2022-06-20 12:10:39 +0900

    Initial Commit

---
+++ H4X0R/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R
+//
+//  Created by output. on 2022/06/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ H4X0R/H4X0RApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RApp.swift
+//  H4X0R
+//
+//  Created by output. on 2022/06/20.
+//
+
+import SwiftUI
+
+@main
+struct H4X0RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
